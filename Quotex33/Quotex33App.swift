c502cf0147502fe99a0ad5ff009cbe86048c1fc9
cxcc <cxzczx>
2022-12-11 23:16:54 +0300

    Initial Commit

---
+++ Quotex33/Quotex33App.swift
@@ -0,0 +1,17 @@
+//
+//  Quotex33App.swift
+//  Quotex33
+//
+//  Created by Вячеслав on 12/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Quotex33App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
